6953a77817d021aa521d2215d05cd468b894b7f3
Naveil <117323603+Naveil@users.noreply.github.com>
2023-05-03 17:35:12 -0700

    Add files via upload

---
+++ main.js
@@ -0,0 +1,12 @@
+var SpeechRecognition = window.webkitSpeechRecognition;
+
+var Recognition = new SpeechRecognition();
+function start(){
+    document.getElementById("textbox").innerHTML = "";
+    Recognition.start();
+}
+
+Recognition.onresult = function(event){
+    console.log(event);
+    
+}
