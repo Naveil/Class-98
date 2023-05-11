d76c12d2ccb0579e07b1049d750dcb58b2cc9680
Naveil <117323603+Naveil@users.noreply.github.com>
2023-05-10 17:40:19 -0700

    Add files via upload

--- main.js
+++ main.js
@@ -8,5 +8,25 @@ function start(){
 
 Recognition.onresult = function(event){
     console.log(event);
+    var content = event.results[0][0].transcript;
+    document.getElementById("textbox").innerHTML = content;
+    console.log(content);
+    speak();
     
+}
+
+camera = document.getElementById("camera");
+Webcam.set({
+    width: 360,
+    height: 250,
+    image_format: 'jpeg',
+    jpeg_quality: 90
+});
+
+function speak(){
+    var synth = window.speechSynthesis;
+    speakdata = document.getElementById("textbox").value;
+    var utterThis = new SpeechSynthesisUtterance(speakdata);
+    synth.speak(utterThis);
+    Webcam.attach(camera);
 }
